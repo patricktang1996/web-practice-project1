9aabaadff2bef324a479a662b4daf27ec623b42d
Patricia Tang <14714930374@163.com>
2024-03-08 23:08:49 +0000

    create class Platform, and add platform to the grid

--- app.js
+++ app.js
@@ -4,17 +4,41 @@ document.addEventListener('DOMContentLoaded', () => {
     let doodlerLeftSpace = 50;
     let doodlerBottomSpace = 150;
     let isGameOver = false;
+    let platformCount = 5;
 
-    function createDoodler(){
+    function createDoodler() {
         grid.appendChild(doodler);
         doodler.classList.add('doodler');
         doodler.style.left= doodlerLeftSpace+'px';
         doodler.style.bottom= doodlerBottomSpace+'px';
     }
 
+    class Platform{
+        constructor(newPlatform){
+            this.bottom = newPlatBottom;
+            this.left = Math.random()*315;
+            this.visual = document.createElement('div');
+
+            const visual = this.visual;
+            visual.classList.add('platform');
+            visual.style.left = this.left+'px';
+            visual.style.bottom = this.bottom+'px';
+            grid.appendChild(visual);
+        }
+    }
+
+    function createPlatforms() {
+        for (let i = 0; i < platformCount; i++){
+            let platformGap = 600 / platformCount;
+            let newPlatBottom = 100 + i * platformGap;
+            let newPlatform = new platformCount(newPlatform);
+        }
+    }
+
     function start(){
         if(!isGameOver){
             createDoodler();
+            createPlatforms();
         }
     }
     //attach to button
